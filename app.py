e1dd4ab606c30a3bad5edde92e93e07b57276251
llforeman <llforeman@users.noreply.github.com>
2025-12-10 01:55:57 +0100

    Fix NameError: flash not defined

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, abort, jsonify, g
+from flask import Flask, render_template, request, redirect, url_for, abort, jsonify, g, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
